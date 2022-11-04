e8d882d1a6eae394818cf55362f11572a5c27959
ZanderOl <olexandruk@gmail.com>
2022-11-04 12:44:21 +0200

    hw 5 and 6 changes

--- js/task-05.js
+++ js/task-05.js
@@ -1,9 +1,15 @@
 const inputEl = document.querySelector("#name-input");
 const outputEl = document.querySelector("#name-output");
 
-inputEl.addEventListener("input", () => {
-  if ((outputEl.textContent = inputEl.value)) {
-    return inputEl.value;
-  }
-  return (outputEl.textContent = "Anonymous");
-});
+// inputEl.addEventListener("input", () => {
+//   if ((outputEl.textContent = inputEl.value)) {
+//     return inputEl.value;
+//   }
+//   return (outputEl.textContent = "Anonymous");
+// });
+
+function onInputChange(event) {
+  outputEl.textContent = event.currentTarget.value || "Anonymous";
+}
+
+inputEl.addEventListener("input", onInputChange);
--- js/task-06.js
+++ js/task-06.js
@@ -1,7 +1,7 @@
 const inputEl = document.querySelector("#validation-input");
 
 inputEl.addEventListener("blur", () => {
-  if (inputEl.value.length.toString() === inputEl.dataset.length) {
+  if (inputEl.value.length === Number(inputEl.dataset.length)) {
     inputEl.classList.remove("invalid");
     inputEl.classList.add("valid");
   } else {
@@ -9,3 +9,5 @@ inputEl.addEventListener("blur", () => {
     inputEl.classList.add("invalid");
   }
 });
+
+// .toString()
